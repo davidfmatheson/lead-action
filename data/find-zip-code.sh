f29cb8770d9958dc37ec9532cfc43a3260da9152
David Matheson <davidfmatheson@gmail.com>
2021-04-26 20:12:22 -0400

    Handy utility for finding correct zip codes

---
+++ data/find-zip-code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+	address="$(pbpaste)"
+else
+	address="$1"
+fi
+
+city=Providence
+state=RI
+echo $address, $city, $state
+curl -s 'https://tools.usps.com/tools/app/ziplookup/zipByAddress' --data "companyName=&address1=$address&city=$city&state=$state" -H "User-Agent: Mozilla/5.0" | jq -r '.addressList[].zip5' | sort -u
+curl -s 'https://tools.usps.com/tools/app/ziplookup/zipByAddress' --data "companyName=&address1=$address&city=$city&state=$state" -H "User-Agent: Mozilla/5.0" | jq 
